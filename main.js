e5ceb3c785653043210026f1588dd86c34bbe1fd
Blake-Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-02-18 16:05:10 -0600

    add computer choice funcationality

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 function getComputerChoice(){
-    
+    const rand = Math.random();
+    let computerChoice = rand;
+    if(rand % 3 === 0){
+        computerChoice = "Rock"
+    } else if(rand % 3 === 1){
+        computerChoice = "Paper"
+    } else { //===2
+        computerChoice = "Scissors"
+    }
+
+    return computerChoice;
 }
 
 function getUserChoice(){
-    
+    console.log("1: Rock\n");
+    console.log("2: Paper\n");
+    console.log("3: Scissors\n");
+
+
 }
 
 function playGame(){
@@ -14,7 +28,7 @@ function playRound(playerSelection, computerSelection){
 
 }
 
-function getWinner(){
+function getWinner(userChoice, computerChoice){
 
 }
 
