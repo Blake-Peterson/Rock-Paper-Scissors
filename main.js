3d35b71b515081b206702fc8ebd9a861874b5d11
Blake-Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-02-18 15:46:53 -0600

    adjust files to main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+function getComputerChoice(){
+    
+}
+
+function getUserChoice(){
+    
+}
+
+function playGame(){
+
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
+
+function getWinner(){
+
+}
+
+
+
+
+function main(){
+
+}
+
