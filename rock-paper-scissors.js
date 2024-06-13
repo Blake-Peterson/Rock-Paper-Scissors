8eed78630f93ae7fcbdd37c459513ec149b9c8b5
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2024-06-13 15:34:17 -0500

    add initial checks for first to 5 wins

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,12 @@
 let humanScore =0;
 let computerScore =0;
 
+const userResultDisplay = document.getElementById("user");
+const computerResultDisplay = document.getElementById("computer");
+
+userResultDisplay.textContent = 0;
+computerResultDisplay.textContent = 0;
+
 function getComputerChoice(){
     let computerChoice ="";
     let random = Math.floor(Math.random() * 3);
@@ -17,15 +23,31 @@ function getComputerChoice(){
 function getHumanChoice(){
     const buttons = document.querySelectorAll("button");
     buttons.forEach((button) => {
-        button.addEventListener("click",()=>{
-            if(button.id === 'rock' ){
-                playRound('rock', getComputerChoice());
-            } else if(button.id === 'paper'){
-                playRound('paper', getComputerChoice());
-            } else if( button.id === 'scissors' ){
-                playRound('scissors', getComputerChoice());
-            }
-        });
+        if(computerScore<5 && humanScore<5){
+            button.addEventListener("click",()=>{
+                if(button.id === 'rock' ){
+                    playRound('rock', getComputerChoice());
+                } else if(button.id === 'paper'){
+                    playRound('paper', getComputerChoice());
+                } else if( button.id === 'scissors' ){
+                    playRound('scissors', getComputerChoice());
+                }
+            });          
+        } else if(humanScore==5) {
+            const p = document.createElement("div");
+            p.classList.add("winner");
+            p.textContent = "User Wins!"+ humanScore + "VS"+ computerScore;
+            const container = document.querySelector("#results-container");
+            container.appendChild(p);
+        } else {
+            const p = document.createElement("div");
+            p.classList.add("winner");
+            p.textContent = "Computer Wins!" +computerScoreScore + "VS"+ humanScoreScore;
+            const container = document.querySelector("#results-container");
+            container.appendChild(p);
+            humanScore =0;
+            computerScore=0;
+        }
     });
 }
 
@@ -35,24 +57,26 @@ function playRound(humanChoice, computerChoice){
     let winner = 0;
     
     if(humanChoice_lower === computerChoice){
-        winner = -1;
-        statement = "You tied! You both chose "+humanChoice_lower;
+        
     } else if (humanChoice_lower === "rock" && computerChoice =="scissors"){
-        winner = 0;
-        statement = "You Win! " + humanChoice_lower + " beats " + computerChoice
+        humanScore++;
+        userResultDisplay.textContent = humanScore;
+        //statement = "You Win! " + humanChoice_lower + " beats " + computerChoice
     } else if(humanChoice_lower === "paper" && computerChoice =="rock") {
-        winner =0;
-        statement = "You Win! " + humanChoice_lower + " beats " + computerChoice
+        humanScore++;
+        userResultDisplay.textContent = humanScore;
+        //statement = "You Win! " + humanChoice_lower + " beats " + computerChoice
     } else if(humanChoice_lower === "scissors" && computerChoice =="paper"){
-        winner = 0;
-        statement = "You Win! " + humanChoice_lower + " beats " + computerChoice;
+        //statement = "You Win! " + humanChoice_lower + " beats " + computerChoice;
+        humanScore++;
+        userResultDisplay.textContent = humanScore;
     } else {
-        winner = 1;
-        statement = "You Lose " + computerChoice + " beats " + humanChoice;
+        computerScore++;
+        computerResultDisplay.textContent = computerScore;
+        //statement = "You Lose " + computerChoice + " beats " + humanChoice;
     }
-    console.log(statement)
-    return winner;
 }
+
 
 /*
 function playGame(){
